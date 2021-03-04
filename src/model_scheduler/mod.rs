4f940c7668b1013eeec4c292240a20c6abc57289
David P. Smith <kcculhwch@gmail.com>
2021-03-03 21:46:05 -0500

    scheduler setup

--- src/model_scheduler/mod.rs
+++ src/model_scheduler/mod.rs
@@ -8,7 +8,7 @@ use std::time::{Duration};
 pub fn run_model_scheduler(mut scheduler: ModelScheduler) -> JoinHandle<()>{
     thread::spawn(move || {
         loop {                
-            scheduler.process_events();
+            scheduler.process_state();
             thread::sleep(Duration::from_millis(5));
         }
     })
@@ -20,34 +20,45 @@ pub fn run_model_scheduler(mut scheduler: ModelScheduler) -> JoinHandle<()>{
 pub struct ModelScheduler {
     models: HashMap<&'static str, Box<dyn Model + Send>>,
     state_tx: Sender<Vec<u8>>,
-    state_rx: Receiver<Vec<u8>>
+    state_rx: Receiver<Vec<u8>>,
+    schedule: Schedule
 }
 
 impl ModelScheduler {
-    pub fn new() -> ModelScheduler {
+    pub fn new(schedule: Schedule) -> ModelScheduler {
         let (sender, receiver) = channel();
         let models: HashMap<&'static str, Box<dyn Model + Send>> = HashMap::new();
         ModelScheduler {
             models,
             state_tx: sender,
-            state_rx: receiver
+            state_rx: receiver,
+            schedule
         }
     }
 
-    pub fn process_events(&mut self) -> bool{
-        true
+    pub fn process_state(&mut self) -> bool{
+        let schedule_fn = self.schedule;
+        match self.state_rx.try_recv() { 
+            Ok(state_vec) => {
+                schedule_fn(&state_vec, &mut self.models);
+                true
+            },
+            Err(_) => (false)
+
+
+        }
+    }
+
+    pub fn register_model(&mut self, name: &'static str, model: Box<dyn Model + Send>) {
+        self.models.insert(name, model);
     }
 }
 
 
 pub trait Model {
-    fn loop_handler(&mut self) {
-
-    }
-
     fn handler(&mut self) {
 
     }
 }
-
-
+#[allow(dead_code)]
+type Schedule = fn(&[u8], &HashMap<&'static str, Box<dyn Model + Send>>);
